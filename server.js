e106d1ad494b8c45c6090f998939ea602a509ac4
Jenna Vuong <jennavuong@gmail.com>
2016-10-05 23:10:37 -0700

    getting there, added setupController.js, working on querying db for record names

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
 const express = require('express');
 const path = require('path');
-// const csv = require('./data/crm.csv');
-const csv = require('csv');
+const csv = require('fast-csv');
 const demoData = require('./db/demo_data.js');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const fs = require('fs');
+const Account = require('./db/models/account.js');
 
 dotenv.config(); // use environment variables
 
 const app = express();
+
 const port = process.env.PORT || 8080;
+
 const dbURI = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ds049436.mlab.com:49436/data-matching-engine`;
 
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
 
-  var readable = fs.createReadStream(__dirname + '/data/crm.csv', { encoding: 'utf8' });
+  var readable = fs.createReadStream(__dirname + '/data/crm.csv');
 
   var writable = fs.createWriteStream(__dirname + '/data/output.txt');
 
-  readable.on('data', function(chunk) {
-    console.log(chunk);
-    writable.write(chunk);
-  });
+  csv
+    .fromStream(readable, { headers: ['name', 'url'] })
+    .on('data', function(data) {
+      
+    })
+    .on('end', function() {
+      console.log('done!');
+    });
+
+  // readable.pipe(writable);
+
 
 	res.sendFile(path.resolve(__dirname), 'index.html');
 });
